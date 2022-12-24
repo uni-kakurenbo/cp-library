4309fa0cb31f8d547fe3061ee328ca4697a3c444
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-12-24 19:59:39 +0900

    Modify: Refactor: val_arrary

--- val_array.hpp
+++ val_array.hpp
@@ -15,7 +15,7 @@ template<class T> struct ValArray : std::valarray<T> {
 
     inline void reserve(const Internal::Size) {}
 
-    template<class I, std::enable_if_t<std::is_same_v<T,typename std::iterator_traits<I>::value_type>,std::nullptr_t> = nullptr>
+    template<class I, std::enable_if_t<std::is_same_v<T,typename std::iterator_traits<I>::value_type>>* = nullptr>
     inline void assign(const I first, const I last) {
         this->resize(std::distance(first, last));
         std::copy(first, last, begin(*this));
