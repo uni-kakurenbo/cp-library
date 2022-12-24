35185ef85526eb0f1e1369e53022c69bd2b2d629
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-12-24 19:55:35 +0900

    Add: New library: random

---
+++ random/generator.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "internal/develop/dev_assert.hpp"
+
+template<class Engine> struct RandomEngine {
+    using result_type = typename Engine::result_type;
+    using signed_result_type = typename std::make_signed_t<result_type>;
+
+  private:
+    mutable Engine engine;
+
+  public:
+    static constexpr result_type MIN = Engine::min();
+    static constexpr result_type MAX = Engine::max();
+
+    static constexpr result_type min() { return MIN; }
+    static constexpr result_type max() { return MAX; }
+
+    constexpr RandomEngine(unsigned long seed = 3141592653UL) { this->engine.seed(seed); };
+
+    inline result_type operator()() const {
+        return this->engine();
+    }
+
+    inline result_type operator()(result_type max) const {
+        if(max == 0) return 0;
+        return (*this)() % max;
+    }
+    inline signed_result_type operator()(signed_result_type min, signed_result_type max) const {
+        dev_assert(min <= max);
+        return min + (*this)(max - min);
+    };
+
+    template<class T = double> inline T real() const {
+        return static_cast<T>(this->engine() + 0.5) / (1.0 + this->max());
+    }
+};
---
+++ random/xorshift.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+struct XorShift {
+    using result_type = std::uint32_t;
+
+    static constexpr result_type MIN = std::numeric_limits<result_type>::min();
+    static constexpr result_type MAX = std::numeric_limits<result_type>::max();
+
+    static constexpr result_type min() { return MIN; }
+    static constexpr result_type max() { return MAX; }
+
+    inline void seed(unsigned int seed) { this->x = seed; }
+
+    constexpr XorShift(uint32_t seed = 3141592653UL) : x(seed) {};
+
+    inline uint32_t operator()() {
+        uint32_t t;
+
+        t = x ^ (x << 11);
+        x = y; y = z; z = w;
+        return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+    }
+
+  private:
+    uint32_t x = 123456789;
+    uint32_t y = 362436069;
+    uint32_t z = 521288629;
+    uint32_t w = 88675123;
+};
