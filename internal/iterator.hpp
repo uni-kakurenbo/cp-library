2b97e55a0637a3fb2381035f2761096fae02f4fa
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-12-25 22:14:47 +0900

    Add: New library: internal iterator

---
+++ internal/iterator.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <iterator>
+
+#include "internal/types.hpp"
+
+namespace Lib {
+
+namespace Internal {
+
+template<class T>
+struct IIterator {
+    using iterator_category = std::output_iterator_tag;
+
+    using difference_type = Size;
+    using value_type = T;
+
+    using pointer = T*;
+    using reference = T&;
+
+    virtual T operator*() const = 0;
+};
+
+template<class T>
+struct IBidirectionalIterator : IIterator<T> {
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    virtual IBidirectionalIterator& operator++() = 0;
+    virtual IBidirectionalIterator& operator--() = 0;
+};
+
+template<class T>
+struct IRandomAccessIterator : IBidirectionalIterator<T> {
+    using iterator_category = std::random_access_iterator_tag;
+    using difference_type = typename IBidirectionalIterator<T>::difference_type;
+
+  public:
+    virtual IRandomAccessIterator& operator+=(const difference_type count) = 0;
+    virtual IRandomAccessIterator& operator-=(const difference_type count) = 0;
+
+    IRandomAccessIterator& operator+(const difference_type count) const {
+        auto res = *this;
+        return res += count;
+    }
+    IRandomAccessIterator& operator-(const difference_type count) const {
+        auto res = *this;
+        return res -= count;
+    }
+};
+
+template<class T, class Container>
+struct IContainerIterator : public IRandomAccessIterator<T> {
+    using difference_type = typename IBidirectionalIterator<T>::difference_type;
+
+  protected:
+    Container *const _ref;
+    difference_type _pos;
+    IContainerIterator(Container *const ref, const difference_type& pos) : _ref(ref), _pos(pos) {}
+
+  public:
+    inline Container* ref() const { return this->_ref; }
+
+    inline difference_type pos() const { return this->_pos; }
+    inline difference_type& pos() { return this->_pos; }
+
+    difference_type operator-(const IContainerIterator& other) const { return this->pos() - other.pos(); }
+
+    inline bool operator<(const IContainerIterator& other) const { return *this - other < 0; }
+    inline bool operator>(const IContainerIterator& other) const { return *this - other > 0; }
+
+    inline bool operator<=(const IContainerIterator& other) const { return not (*this > other); }
+    inline bool operator>=(const IContainerIterator& other) const { return not (*this < other); }
+
+    inline bool operator!=(const IContainerIterator& other) const { return this->ref() != other.ref() or *this < other or *this > other; }
+    inline bool operator==(const IContainerIterator& other) const { return not (*this != other); }
+};
+
+
+} // namespace Internal
+
+} // namespace Lib
