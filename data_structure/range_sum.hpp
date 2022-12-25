204dd4845b1a8c288c5c1549bd3229c849f87a88
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-12-25 22:16:08 +0900

    Modify: Add: iterator

--- data_structure/range_sum.hpp
+++ data_structure/range_sum.hpp
@@ -6,16 +6,20 @@
 #include <atcoder/fenwicktree>
 
 #include "template.hpp"
+
 #include "internal/develop/dev_assert.hpp"
+#include "internal/iterator.hpp"
+#include "internal/types.hpp"
 
 namespace Lib {
 
+
 template<class T> struct RangeSum : atcoder::fenwick_tree<T> {
   protected:
-    int _n;
+    Internal::Size _n;
 
   public:
-    RangeSum(const int n = 0) : atcoder::fenwick_tree<T>(n+1) { this->_n = n; }
+    RangeSum(const Internal::Size n = 0) : atcoder::fenwick_tree<T>(n+1) { this->_n = n; }
     RangeSum(std::initializer_list<T> init_list) : RangeSum(ALL(init_list)) {}
 
     template<class I>
@@ -23,24 +27,40 @@ template<class T> struct RangeSum : atcoder::fenwick_tree<T> {
         for(auto itr=first; itr!=last; ++itr) this->set(itr-first, *itr);
     }
 
-    inline int size() const {
+    inline Internal::Size size() const {
         return this->_n;
     }
-    inline T get(const int p) {
+    inline T get(const Internal::Size p) {
         dev_assert(0 <= p and p < this->size());
         return this->sum(p, p+1);
     }
 
-    inline void set(const int p, const T x) {
+    inline void set(const Internal::Size p, const T x) {
         dev_assert(0 <= p and p < this->size());
         this->add(p, x - this->get(p));
     }
 
-    inline std::vector<T> _debug() {
-        std::vector<T> res(this->size());
-        REP(i, this->size()) res[i] = this->get(i);
-        return res;
-    }
+
+    struct Iterator : virtual Internal::IContainerIterator<T,RangeSum> {
+        using difference_type = typename Internal::IContainerIterator<T,RangeSum>::difference_type;
+
+      public:
+        Iterator(RangeSum *const ref, const Internal::Size pos) : Internal::IContainerIterator<T,RangeSum>(ref, pos) {}
+
+        T operator*() const override { return this->ref()->get(this->pos()); }
+
+        Iterator& operator++() override { return ++this->pos(), *this; }
+        Iterator& operator--() override { return --this->pos(), *this; }
+
+        Iterator& operator+=(const difference_type count) override { return this->pos() += count, *this; }
+        Iterator& operator-=(const difference_type count) override { return this->pos() -= count, *this; }
+
+    };
+    using iterator = Iterator;
+
+    Iterator begin() { return Iterator(this, 0); }
+    Iterator end() { return Iterator(this, this->size()); }
 };
+
 
 } // namespace Lib
