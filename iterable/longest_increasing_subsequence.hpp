bb06910176a53e709df82da33bdd987022921a2f
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-01-06 23:09:10 +0900

    Add: New Library: LIS

---
+++ iterable/longest_increasing_subsequence.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+#include "internal/types.hpp"
+#include "grid.hpp"
+
+
+namespace lib {
+
+
+template<class T, bool STRICT, class container = std::vector<T>>
+struct lis : container {
+    using size_type = typename internal::size_t;
+
+    std::vector<T> indices;
+
+    lis() {}
+    template<class I> lis(const I first, const I last) {
+        std::vector<size_type> positions(std::distance(first, last), -1);
+
+        size_type pos = 0;
+        for(auto itr=first; itr!=last; ++pos, ++itr) {
+            typename container::iterator bound;
+
+            if constexpr(STRICT) bound = std::lower_bound(std::begin(*this), std::end(*this), *itr);
+            else bound = std::upper_bound(std::begin(*this), std::end(*this), *itr);
+
+            positions[pos] = std::distance(std::begin(*this), bound);
+
+            if(std::end(*this) == bound) this->emplace_back(*itr);
+            else *bound = *itr;
+        }
+
+        size_type target = *std::max_element(positions.begin(), positions.end());
+        for(size_type i = positions.size(); --i >= 0;){
+            if(positions[i] == target){
+                (*this)[target] = *(first + i);
+                this->indices.emplace_back(i);
+                --target;
+            }
+        }
+        std::reverse(this->indices.begin(), this->indices.end());
+    }
+};
+
+
+} // namespace lib
