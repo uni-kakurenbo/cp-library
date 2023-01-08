d9519b92945d72a1abf1972cd461f3d1a302e791
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-01-08 09:42:19 +0900

    Add: New library: dsu

---
+++ data_structure/disjoint_set_union.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+
+#include "internal/dev_assert.hpp"
+#include "internal/types.hpp"
+// #include <atcoder/dsu>
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+
+namespace lib {
+
+//Thanks to: atcoder::dsu
+struct dsu {
+    using size_type = internal::size_t;
+
+  private:
+    size_type _n, _group_count;
+    // root node: -1 * component size
+    // otherwise: parent
+    mutable std::vector<size_type> _parent_or_size;
+
+  public:
+    dsu() : _n(0) {}
+    explicit dsu(const size_type n) : _n(n), _group_count(n), _parent_or_size(n, -1) {}
+
+    inline size_type size() const { return this->_n; }
+    inline size_type group_count() const { return this->_group_count; }
+
+    inline size_type merge(const size_type a, const size_type b) {
+        dev_assert(0 <= a && a < _n);
+        dev_assert(0 <= b && b < _n);
+        size_type x = this->leader(a), y = this->leader(b);
+        if (x == y) return x;
+        --this->_group_count;
+        if (-this->_parent_or_size[x] < -this->_parent_or_size[y]) std::swap(x, y);
+        this->_parent_or_size[x] += this->_parent_or_size[y];
+        this->_parent_or_size[y] = x;
+        return x;
+    }
+
+    inline bool same(const size_type a, const size_type b) const {
+        dev_assert(0 <= a && a < _n);
+        dev_assert(0 <= b && b < _n);
+        return this->leader(a) == this->leader(b);
+    }
+
+    inline size_type leader(const size_type a) const {
+        dev_assert(0 <= a && a < _n);
+        if (_parent_or_size[a] < 0) return a;
+        return _parent_or_size[a] = this->leader(_parent_or_size[a]);
+    }
+
+    inline size_type size(const size_type a) const {
+        dev_assert(0 <= a && a < _n);
+        return -_parent_or_size[this->leader(a)];
+    }
+
+    inline std::vector<std::vector<size_type>> groups() const {
+        std::vector<size_type> leader_buf(_n), group_size(_n);
+        for (size_type i = 0; i < _n; i++) {
+            leader_buf[i] = this->leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<size_type>> result(_n);
+        for (size_type i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (size_type i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<size_type>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+};
+
+
+} // namespace lib
