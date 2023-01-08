fa1adfe66a5cea456c49db0fffaa255f769459d8
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-01-08 20:27:09 +0900

    Update: Verify: Wavelet Matrix

---
+++ verify/range_kth_smallest/00.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "template.hpp"
+#include "data_structure/wavelet_matrix.hpp"
+
+signed main() {
+    int n, q; std::cin >> n >> q;
+    std::vector<int> a(n); input >> a;
+
+    lib::wavelet_matrix<int> data(ALL(a));
+    debug(data);
+
+    REP(q) {
+        int l, r, k; std::cin >> l >> r >> k;
+        print(data.kth_smallest(l, r, k));
+    }
+}
---
+++ verify/static_range_frequency/00.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "template.hpp"
+#include "data_structure/wavelet_matrix.hpp"
+
+signed main() {
+    int n, q; std::cin >> n >> q;
+    std::vector<int> a(n); input >> a;
+
+    lib::wavelet_matrix<int> data(ALL(a));
+    debug(data);
+
+    REP(q) {
+        int l, r, x; std::cin >> l >> r >> x;
+        print(data.count(l, r, x));
+    }
+}
