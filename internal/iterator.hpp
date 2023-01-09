d1919ab053272d774aad06ccfc6b70897249df23
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-01-09 14:38:50 +0900

    Refactor: internal/iterator

--- internal/iterator.hpp
+++ internal/iterator.hpp
@@ -22,17 +22,17 @@ struct iterator_interface {
 };
 
 template<class T>
-struct bidirectiona_Iterator_interface : iterator_interface<T> {
+struct bidirectiona_iterator_interface : iterator_interface<T> {
     using iterator_category = std::bidirectional_iterator_tag;
 
-    virtual bidirectiona_Iterator_interface& operator++() = 0;
-    virtual bidirectiona_Iterator_interface& operator--() = 0;
+    virtual bidirectiona_iterator_interface& operator++() = 0;
+    virtual bidirectiona_iterator_interface& operator--() = 0;
 };
 
 template<class T>
-struct random_access_iterator_base : bidirectiona_Iterator_interface<T> {
+struct random_access_iterator_base : bidirectiona_iterator_interface<T> {
     using iterator_category = std::random_access_iterator_tag;
-    using difference_type = typename bidirectiona_Iterator_interface<T>::difference_type;
+    using difference_type = typename bidirectiona_iterator_interface<T>::difference_type;
 
   public:
     virtual random_access_iterator_base& operator+=(const difference_type count) = 0;
@@ -41,7 +41,7 @@ struct random_access_iterator_base : bidirectiona_Iterator_interface<T> {
 
 template<class T, class container>
 struct container_iterator_interface : public random_access_iterator_base<T> {
-    using difference_type = typename bidirectiona_Iterator_interface<T>::difference_type;
+    using difference_type = typename bidirectiona_iterator_interface<T>::difference_type;
 
   protected:
     const container *const _ref;
