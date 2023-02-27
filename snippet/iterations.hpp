6f1b94c7f5319355b3abcffebae7dadd3d10435b
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-02-27 21:38:21 +0900

    Update: Library: snippet/iterations.hpp

--- snippet/iterations.hpp
+++ snippet/iterations.hpp
@@ -5,8 +5,8 @@
 #define LOOP(n) REPI($_, (n))
 
 #define REPI(i,n) for(int i=0, i##_length=int(n); i<i##_length; ++i)
-#define REPF(i,l,r) for(auto i=(l), i##_last=(r); i!=i##_last; ++i)
-#define REPIS(i,l,r,s) for(auto i=(l), i##_last=(r); i!=i##_last; i+=(s))
+#define REPF(i,l,r) for(auto i=(l), i##_last=(r); i<i##_last; ++i)
+#define REPIS(i,l,r,s) for(auto i=(l), i##_last=(r); i<i##_last; i+=(s))
 
 #define REPR(i,n) for(auto i=(n); --i>=0;)
 #define REPB(i,l,r) for(auto i=(r), i##_last=(l); --i>=i##_last;)
