01adfd1d65756bf8e5e622762159a465b55f2593
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-04-19 17:22:27 +0900

    Modify: graph/spanning_tree.hpp

--- graph/spanning_tree.hpp
+++ graph/spanning_tree.hpp
@@ -12,7 +12,7 @@ namespace graph_impl {
 
 
 template<class G, class cost_type, class Compare>
-cost_type kruskal(const G& graph, const Compare compare) {
+cost_type kruskal(const G& graph, const Compare compare, G *const mst = nullptr) {
     atcoder::dsu ds(graph.size());
 
     std::vector<std::tuple<typename G::edge::cost_type,size_t,size_t>> edges;
@@ -23,10 +23,13 @@ cost_type kruskal(const G& graph, const Compare compare) {
 
     std::sort(ALL(edges), compare);
 
+    if(mst) mst->clear(), mst->resize(graph.size());
+
     cost_type res = cost_type{};
     ITR(w, u, v, edges) {
         if(not ds.same(u, v)) {
             ds.merge(u, v);
+            if(mst) mst->add_edge_bidirectionally(u, v, w);
             res += w;
         }
     }
@@ -42,14 +45,14 @@ cost_type kruskal(const G& graph, const Compare compare) {
 
 template<class edge_cost>
 template<class cost_type>
-cost_type graph<edge_cost>::minimum_spanning_tree_cost() const {
-    return internal::graph_impl::kruskal<graph<edge_cost>,cost_type,std::less<tuple<cost_type,size_type,size_type>>>(*this, {});
+cost_type graph<edge_cost>::minimum_spanning_tree(graph<edge_cost> *const mst) const {
+    return internal::graph_impl::kruskal<graph<edge_cost>,cost_type,std::less<tuple<cost_type,size_type,size_type>>>(*this, {}, mst);
 }
 
 template<class edge_cost>
 template<class cost_type>
-inline cost_type graph<edge_cost>::maximum_spanning_tree_cost() const {
-    return internal::graph_impl::kruskal<graph<edge_cost>,cost_type,std::greater<tuple<cost_type,size_type,size_type>>>(*this, {});
+inline cost_type graph<edge_cost>::maximum_spanning_tree(graph<edge_cost> *const mst) const {
+    return internal::graph_impl::kruskal<graph<edge_cost>,cost_type,std::greater<tuple<cost_type,size_type,size_type>>>(*this, {}, mst);
 }
 
 
