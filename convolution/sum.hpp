b23214e0be11acdca483643c35a10a5282697a9f
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-06-06 16:55:36 +0900

    Update: Library: convolution

--- convolution/sum.hpp
+++ convolution/sum.hpp
@@ -12,14 +12,14 @@ using atcoder::convolution_ll;
 
 
 template<class I1, class I2>
-auto convolution(const I1 a_frist, const I1 a_last, const I2 b_frist, const I2 b_last) {
+auto convolution(const I1 a_first, const I1 a_last, const I2 b_first, const I2 b_last) {
     std::vector<typename std::iterator_traits<I1>::value_type> a(a_first, a_last);
     std::vector<typename std::iterator_traits<I2>::value_type> b(b_first, b_last);
     return atcoder::convolution(a, b);
 }
 
 template<class I1, class I2>
-auto convolution_ll(const I1 a_frist, const I1 a_last, const I2 b_frist, const I2 b_last) {
+auto convolution_ll(const I1 a_first, const I1 a_last, const I2 b_first, const I2 b_last) {
     std::vector<typename std::iterator_traits<I1>::value_type> a(a_first, a_last);
     std::vector<typename std::iterator_traits<I2>::value_type> b(b_first, b_last);
     return atcoder::convolution_ll(a, b);
