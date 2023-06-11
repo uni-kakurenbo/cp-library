d92336446d62537071c0f2f8cd150abc0065f4e6
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-06-11 23:55:47 +0900

    Update: Library: timer

---
+++ timer.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+
+#include <cstdint>
+#include <chrono>
+#include <algorithm>
+
+
+namespace lib {
+
+
+struct timer {
+    using time = std::int32_t;
+    using progress_type = long double;
+
+  protected:
+    const time time_limit = 0;
+    const progress_type progress_duration = 0;
+
+    std::chrono::system_clock::time_point clock_start, clock_end;
+
+  public:
+
+    timer(const time time_limit = 0) noexcept : time_limit(time_limit),
+        progress_duration(
+            static_cast<progress_type>(
+                std::chrono::duration_cast<std::chrono::system_clock::duration>(
+                    std::chrono::milliseconds(time_limit)
+                ).count()
+            )
+        )
+    {
+        this->reset();
+    }
+
+    inline time limit() noexcept { return this->time_limit; }
+
+    inline timer* reset() noexcept {
+        this->clock_start = std::chrono::system_clock::now();
+        this->clock_end = clock_start + std::chrono::milliseconds(this->time_limit);
+        return this;
+    }
+
+    inline time elapsed() const noexcept {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - this->clock_start).count();
+    }
+    inline time remaining () const noexcept {
+        return time_limit - this->elapsed();
+    }
+
+    inline bool expired() const noexcept { return this->elapsed() >= time_limit; }
+    inline progress_type progress() const noexcept {
+        return std::clamp<progress_type>((std::chrono::system_clock::now() - this->clock_start).count() / this->progress_duration, 0, 1);
+    }
+};
+
+
+} // namespace lib
