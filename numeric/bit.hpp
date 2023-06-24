7a02ff5ade9b4da71c643783b06461ac7b9ae136
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-06-24 20:46:33 +0900

    Update: Library: numeric: bit

--- numeric/bit.hpp
+++ numeric/bit.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 
+#include <immintrin.h>
+
 #include <type_traits>
 #include <cstdint>
 #include <limits>
@@ -53,7 +55,7 @@ __attribute__((target("bmi,bmi2,lzcnt"))) inline constexpr int countl_zero(const
         const ull high = v >> DIGITS_ULL;
         const ull low = v & MAX_ULL;
 
-        if(high > 0) return __builtin_clzll(high) - (DIGITS_ULL << 1) + DIGITS;
+        if(high != 0) return __builtin_clzll(high) - (DIGITS_ULL << 1) + DIGITS;
         return  __builtin_clzll(low) - DIGITS_ULL + DIGITS;
     }
 }
@@ -96,7 +98,7 @@ inline constexpr int lowest_bit_pos(const T v) noexcept(NO_EXCEPT) {
         const ull high = v >> DIGITS_ULL;
         const ull low = v & MAX_ULL;
 
-        if(low > 0) return __builtin_ffsll(low) - 1;
+        if(low != 0) return __builtin_ffsll(low) - 1;
         return __builtin_ffsll(high) + DIGITS_ULL - 1;
     }
 }
@@ -120,6 +122,25 @@ inline constexpr T bit_ceil(const T v) noexcept(NO_EXCEPT) {
     }
 }
 
+template<class T>
+__attribute__((target("bmi2"))) inline constexpr T clear_higher_bits(const T v, const T p) {
+    LIB_STATUC_ASSERT_UNSIGNED(T);
+    constexpr int DIGITS = std::numeric_limits<T>::digits;
+    assert(p < DIGITS);
+    if constexpr(DIGITS <= 32) return _bzhi_u64(v, p);
+    if constexpr(DIGITS <= 64) return _bzhi_u64(v, p);
+    else {
+        static_assert(DIGITS <= 128);
+
+        constexpr std::uint64_t MAX64 = std::numeric_limits<std::uint64_t>::max();
+
+        const std::uint64_t high = v >> 64;
+        const std::uint64_t low = v & MAX64;
+
+        if(p < 64) return _bzhi_u64(low, p);
+        return low | (T{_bzhi_u64(high, p - 64)} << 64);
+    }
+}
 
 #undef LIB_STATUC_ASSERT_UNSIGNED
 
