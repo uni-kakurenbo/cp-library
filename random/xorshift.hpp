a2e3d7480864a056e22f47a1b4c08b2a79f6b9ce
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-07-28 23:03:58 +0900

    Update: Library: random: xorshift

--- random/xorshift.hpp
+++ random/xorshift.hpp
@@ -23,19 +23,19 @@ struct xorshift {
     constexpr xorshift() {};
     constexpr xorshift(const std::uint32_t seed) noexcept(NO_EXCEPT) : w(seed) {};
 
-    inline std::uint32_t operator()() noexcept(NO_EXCEPT) {
-        std::uint32_t t;
-
+    inline result_type gen() noexcept(NO_EXCEPT) {
+        result_type t;
         t = x ^ (x << 11);
         x = y; y = z; z = w;
         return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
     }
+    inline result_type operator()() noexcept(NO_EXCEPT) { return this->gen(); }
 
   private:
-    std::uint32_t x = 123456789;
-    std::uint32_t y = 362436069;
-    std::uint32_t z = 521288629;
-    std::uint32_t w = 88675123;
+    result_type x = 123456789;
+    result_type y = 362436069;
+    result_type z = 521288629;
+    result_type w = 88675123;
 };
 
 struct xorshift64 {
@@ -52,10 +52,11 @@ struct xorshift64 {
     constexpr xorshift64() noexcept(NO_EXCEPT) {};
     constexpr xorshift64(const std::uint64_t seed) noexcept(NO_EXCEPT) : x(seed) {};
 
-    inline std::uint64_t operator()() noexcept(NO_EXCEPT) {
-        x = x ^ (x << 13), x = x ^ (x <<  7), x = x ^ (x << 17);
+    inline result_type gen() noexcept(NO_EXCEPT) {
+        x = x ^ (x << 13), x = x ^ (x >> 7), x = x ^ (x << 17);
         return x;
     }
+    inline result_type operator()() noexcept(NO_EXCEPT) { return this->gen(); }
 
   private:
     std::uint64_t x = 3141592653589793238UL;
