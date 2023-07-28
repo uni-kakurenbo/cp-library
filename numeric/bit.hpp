7e64c4949ccf17374991565661062c496aada343
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-07-28 23:03:27 +0900

    Update: Library: numeric: bit

--- numeric/bit.hpp
+++ numeric/bit.hpp
@@ -110,6 +110,10 @@ inline constexpr int countr_zero(const T v) noexcept(NO_EXCEPT) {
     return lowest_bit_pos(v);
 }
 
+template<class T> constexpr int countl_one(const T x) { return countl_zero<T>(~x); }
+
+template<class T> constexpr int countr_one(const T x) { return countr_zero<T>(~x); }
+
 
 template<class T>
 inline constexpr T bit_ceil(const T v) noexcept(NO_EXCEPT) {
@@ -140,6 +144,58 @@ __attribute__((target("bmi2"))) inline constexpr T clear_higher_bits(const T v, 
         if(p < 64) return _bzhi_u64(low, p);
         return low | (T{_bzhi_u64(high, p - 64)} << 64);
     }
+}
+
+template<class T> constexpr bool has_single_bit(const T x) {
+    LIB_STATUC_ASSERT_UNSIGNED(T);
+    return (x != 0) and (x & (x - 1)) == 0;
+}
+
+template<class T> constexpr T shiftl(const T, const int);
+template<class T> constexpr T shiftr(const T, const int);
+
+template<class T> constexpr T shiftl(const T x, const int n) {
+    LIB_STATUC_ASSERT_UNSIGNED(T);
+    constexpr int DIGITS = std::numeric_limits<T>::digits;
+    if(n < 0) return shiftr(x, -n);
+    if(n >= DIGITS) return 0;
+    return x << n;
+}
+
+template<class T> constexpr T shiftr(const T x, const int n) {
+    LIB_STATUC_ASSERT_UNSIGNED(T);
+    constexpr int DIGITS = std::numeric_limits<T>::digits;
+    if(n < 0) return shiftl(x, -n);
+    if(n >= DIGITS) return 0;
+    return x >> n;
+}
+
+template<class T> constexpr T rotl(const T x, const int n) {
+    LIB_STATUC_ASSERT_UNSIGNED(T);
+    constexpr int DIGITS = std::numeric_limits<T>::digits;
+    if constexpr(has_single_bit(DIGITS)) {
+        constexpr unsigned U_DIGITS = static_cast<unsigned>(DIGITS);
+        const unsigned r = static_cast<unsigned>(n);
+        return (x << (r % U_DIGITS)) | (x >> ((-r) % U_DIGITS));
+    }
+    const int r = n % DIGITS;
+    if(r == 0) return x;
+    if(r > 0) return (x << r) | (x >> ((DIGITS - r) % DIGITS));
+    return (x >> (-r)) | (x << ((DIGITS + r) % DIGITS)); // rotr
+}
+
+template<class T> constexpr T rotr(const T x, const int n) {
+    LIB_STATUC_ASSERT_UNSIGNED(T);
+    constexpr int DIGITS = std::numeric_limits<T>::digits;
+    if constexpr(has_single_bit(DIGITS)) {
+        constexpr unsigned U_DIGITS = static_cast<unsigned>(DIGITS);
+        const unsigned r = static_cast<unsigned>(n);
+        return (x >> (r % U_DIGITS)) | (x << ((-r) % U_DIGITS)); // rotl
+    }
+    const int r = n % DIGITS;
+    if(r == 0) return x;
+    if(r > 0) return (x >> r) | (x << ((DIGITS - r) % DIGITS));
+    return (x << (-r)) | (x >> ((DIGITS + r) % DIGITS));
 }
 
 #undef LIB_STATUC_ASSERT_UNSIGNED
