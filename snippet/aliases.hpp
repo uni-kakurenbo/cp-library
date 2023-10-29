d284f89ca6e4056920376dab873f98590565659a
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-10-29 16:52:20 +0900

    macros: CONTINUE, BREAK

--- snippet/aliases.hpp
+++ snippet/aliases.hpp
@@ -13,6 +13,8 @@
 
 #define until(...) while(!(__VA_ARGS__))
 
+#define CONTINUE(...) { __VA_ARGS__; continue; }
+#define BREAK(...) { __VA_ARGS__; break; }
 
 #define ALL(x) std::ranges::begin((x)),std::ranges::end((x))
 #define RALL(x) std::ranges::rbegin((x)),std::ranges::rend((x))
