44ab4e80bf72a4b64c28b092db47a5e4347c1806
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-11-05 21:28:49 +0900

    workflows: verify

---
+++ verify.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+dropbox_token="$(
+    curl https://api.dropbox.com/oauth2/token \
+        -d grant_type=refresh_token \
+        -d "$DROPBOX_REFRESH_TOKEN" \
+        -u "$DROPBOX_APP_KEY":"$DROPBOX_APP_SECRET" \
+        -s \
+    | jq -r ".access_token"
+)"
+
+export DROPBOX_TOKEN=$dropbox_token
+oj-verify all --config-file=".verify-helper/remote-config.toml"
