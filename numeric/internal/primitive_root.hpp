24a8a2f59897fe93ba5b897def57c87c57971d3b
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-05 17:45:23 +0900

    Update primitive_root.hpp

--- numeric/internal/primitive_root.hpp
+++ numeric/internal/primitive_root.hpp
@@ -26,6 +26,9 @@ constexpr u64 primitive_root(const u64 p) noexcept(NO_EXCEPT) {
         bool ok = true;
         ITR(div, divs) {
             const Mint r = Mint{ x }.pow(div);
+	    std::cout << x.val() << std::endl;
+	    std::cout << r.val() << std::endl;
+	    std::cout << div << std::endl;
 	    if (r == one && div != p - 1) {
 		ok = false;
 		break;
