8d323a52bd27c66ff91879fd6df317a1814bbfb4
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-05 13:29:35 +0900

    Update primitive_root.hpp

--- numeric/internal/primitive_root.hpp
+++ numeric/internal/primitive_root.hpp
@@ -16,25 +16,25 @@ namespace internal {
 
 template<modint_family Mint>
 constexpr u64 primitive_root(const u64 p) noexcept(NO_EXCEPT) {
-  if constexpr(dynamic_modint_family<Mint>) Mint::set_mod(p);
-  assert(Mint::mod() == p);
-  
-	const auto divs = divisor(p - 1);
+    if constexpr(dynamic_modint_family<Mint>) Mint::set_mod(p);
+    assert(Mint::mod() == p);
 
-  const Mint one = Mint::raw(1), rev = Mint::raw(p - 1);
-  REP(x, 1, p) {
-		bool ok = true;
-    ITR(div, divs) {
-      const Mint r = Mint{ i }.pow(div);
-			if (r == one && v != rev) {
-				ok = false;
-				break;
-			}
+    const auto divs = divisors(p - 1);
+
+    const Mint one = Mint::raw(1), rev = Mint::raw(p - 1);
+    REP(x, 1, p) {
+        bool ok = true;
+        ITR(div, divs) {
+            const Mint r = Mint{ x }.pow(div);
+	        if (r == one && v != rev) {
+		    ok = false;
+		    break;
 		}
-		if(ok) return i;
-	}
+        }
+	if(ok) return i;
+    }
 
-  assert(false);
+    assert(false);
 }
 
 
