2c77397cc13d8f56c7c9bbe70e9b946bef715f43
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-05 13:51:55 +0900

    Create 00.test.cpp

---
+++ verify/primitive-root/00.test.cpp
@@ -0,0 +1,25 @@
+/*
+ * @uni_kakurenbo
+ * https://github.com/uni-kakurenbo/competitive-programming-workspace
+ *
+ * CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+/* #language C++ 20 GCC */
+
+
+#define PROBLEM "https://judge.yosupo.jp/problem/factorize"
+
+#include "sneaky/enforce_int128_enable.hpp"
+
+#include "snippet/fast_io.hpp"
+#include "snippet/iterations.hpp"
+#include "adapter/io.hpp"
+#include "numeric/fast_prime.hpp"
+
+signed main() {
+    int q; std::cin >> q;
+    REP(q) {
+        lib::i64 p; std::cin >> p;
+        print(lib::primitive_root(p));
+    }
+}
