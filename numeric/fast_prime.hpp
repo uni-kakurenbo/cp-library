d7e1adcccdc954cd95de9959284e3a90a436b09c
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-05 11:57:20 +0900

    Update fast_prime.hpp

--- numeric/fast_prime.hpp
+++ numeric/fast_prime.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+
 #include "numeric/internal/primality_test.hpp"
 #include "numeric/internal/factorize.hpp"
 #include "numeric/internal/divisors.hpp"
+
+
+namespace lib {
+
+
+constexpr i64 INTERNAL_MODINT_ID = -(1UL << 62);
+
+
+inline constexpr bool is_prime(const u64 n) noexcept(NO_EXCEPT) {
+    return is_prime<dynamic_modint_32bit<INTERNAL_MODINT_ID>, dynamic_modint_64bit<INTERNAL_MODINT_ID>>(n);
+}
+
+
+template<u64 N>
+inline constexpr bool is_prime() noexcept(NO_EXCEPT) {
+    return is_prime<static_modint_32bit<N>, static_modint_64bit<N>>(N);
+}
+
+
+} // namespace lib
