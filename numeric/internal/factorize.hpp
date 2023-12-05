3d4f065d9cd9f91dc189ba768872378688a4b3cb
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-05 14:38:27 +0900

    Update factorize.hpp

--- numeric/internal/factorize.hpp
+++ numeric/internal/factorize.hpp
@@ -62,13 +62,13 @@ T find_factor(const T n) noexcept(NO_EXCEPT) {
             for(int k = 0; g == 1 && k < r; k += m) {
                 ys = y;
                 for(int i = 0; i < m && i < r - k; ++i) q *= x - (y = f(y));
-                g = binary_gcd(q.val(), n);
+                g = lib::binary_gcd(q.val(), n);
             }
         }
 
         if(g == n) {
             do {
-                g = binary_gcd((x - (ys = f(ys))).val(), n);
+                g = lib::binary_gcd((x - (ys = f(ys))).val(), n);
             } while(g == 1);
         }
         if(g != n) return g;
