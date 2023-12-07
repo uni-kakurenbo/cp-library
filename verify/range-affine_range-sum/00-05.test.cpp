00e1d0a62f690cb6f8b50b0d6bc5c4c430c289aa
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-07 16:53:19 +0900

    Create 00-05.test.cpp

---
+++ verify/range-affine_range-sum/00-05.test.cpp
@@ -0,0 +1,40 @@
+/*
+ * @uni_kakurenbo
+ * https://github.com/uni-kakurenbo/competitive-programming-workspace
+ *
+ * CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+/* #language C++ 20 GCC */
+
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include <iostream>
+#include "snippet/aliases.hpp"
+#include "snippet/fast_io.hpp"
+#include "snippet/iterations.hpp"
+#include "numeric/modint.hpp"
+#include "adapter/io.hpp"
+#include "data_structure/lazy_segment_tree.hpp"
+#include "action/range_affine_range_sum.hpp"
+
+
+signed main() {
+    lib::montgmoery_modint_64bit<-1>::set_mod(998244353);
+
+    int n, q; std::cin >> n >> q;
+    std::vector<lib::montgmoery_modint_64bit<-1>> a(n); input >> a;
+
+    lib::lazy_segment_tree<lib::actions::range_affine_range_sum<lib::montgmoery_modint_64bit<-1>>> data(ALL(a));
+
+    REP(q) {
+        int t; std::cin >> t;
+        if(t == 0) {
+            int l, r, b, c; std::cin >> l >> r >> b >> c;
+            data(l, r) += { b, c };
+        }
+        if(t == 1) {
+            int l, r; std::cin >> l >> r;
+            print(data(l, r).fold());
+        }
+    }
+}
