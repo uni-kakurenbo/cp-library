81ff9d7de2a0ae676396bf93ca060f2d1e18f453
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-08 09:36:47 +0900

    Update binomial_coefficient.hpp

--- numeric/binomial_coefficient.hpp
+++ numeric/binomial_coefficient.hpp
@@ -12,17 +12,17 @@
 #include "snippet/iterations.hpp"
 
 #include "numeric/internal/barrett.hpp"
-#include "numeric/internal/modint_interface.hpp"
+
+#include "numeric/modint.hpp"
 
 
-
-// Thanks to: https://nyaannyaan.github.io/library/modulo/arbitrary-mod-binomial.hpp
 namespace lib {
 
 namespace internal {
 
 
-template<class T, class R = T>
+// Thanks to: https://nyaannyaan.github.io/library/modulo/arbitrary-mod-binomial.hpp
+template<class T, class R = T, Mint = barrett_modint_32bit<-(1L << 62) + 4>>
     requires (std::numeric_limits<R>::digits > 30) || modint_family<R>
 struct binomial_coefficient_prime_power_mod {
     using value_type = T;
@@ -33,11 +33,12 @@ struct binomial_coefficient_prime_power_mod {
 
   private:
     using self = binomial_coefficient_prime_power_mod;
+    using mint = Mint;
 
     u32 _p, _q, _m;
     value_type _max;
-    std::valarray<u32> _fact, _inv_fact, _inv;
-    u32 _delta;
+    std::valarray<mint> _fact, _inv_fact, _inv;
+    mint _delta;
     barrett_32bit _barrett_m, _barrett_p;
 
     static constexpr std::pair<u32,u32> _factorize(u32 m) {
@@ -54,10 +55,11 @@ struct binomial_coefficient_prime_power_mod {
     }
 
     void _init() {
+        mint::set_mod(this->_m);
         this->_barrett_m = barrett_32bit(this->_m);
         this->_barrett_p = barrett_32bit(this->_p);
 
-        this->_delta = (this->_p == 2 and this->_q >= 3) ? 1 : this->_m - 1;
+        this->_delta = mint::raw((this->_p == 2 and this->_q >= 3) ? 1 : this->_m - 1);
 
         const u32 size = std::min(this->_m, static_cast<u32>(this->_max) + 1);
         assert(size < self::MAX_BUFFER_SIZE);
@@ -66,23 +68,23 @@ struct binomial_coefficient_prime_power_mod {
         this->_inv_fact.resize(size);
         this->_inv.resize(size);
 
-        this->_fact[0] = this->_inv_fact[0] = this->_inv[0] = 1;
-        this->_fact[1] = this->_inv_fact[1] = this->_inv[1] = 1;
+        this->_fact[0] = this->_inv_fact[0] = this->_inv[0] = mint::one;
+        this->_fact[1] = this->_inv_fact[1] = this->_inv[1] = mint::one;
 
         REP(i, 2, size) {
             if(i % this->_p == 0) {
                 this->_fact[i] = this->_fact[i - 1];
-                this->_fact[i + 1] = this->_barrett_m.remainder(static_cast<u64>(this->_fact[i - 1]) * (i + 1));
+                this->_fact[i + 1] = this->_fact[i - 1] * (i + 1);
                 ++i;
             }
             else {
-                this->_fact[i] = this->_barrett_m.remainder(static_cast<u64>(this->_fact[i - 1]) * i);
+                this->_fact[i] = this->_fact[i - 1] * i;
             }
         }
 
-        this->_inv_fact[size - 1] = this->_barrett_m.pow(static_cast<u64>(this->_fact[size - 1]), this->_m / this->_p * (this->_p - 1) - 1);
+        this->_inv_fact[size - 1] = this->_fact[size - 1].pow(this->_m / this->_p * (this->_p - 1) - 1);
         REPD(i, 2, size - 1) {
-            this->_inv_fact[i] = this->_barrett_m.remainder(static_cast<u64>(this->_inv_fact[i + 1]) * (i + 1));
+            this->_inv_fact[i] = this->_inv_fact[i + 1] * (i + 1);
             if(i % this->_p == 0) {
                 this->_inv_fact[i - 1] = this->_inv_fact[i];
                 --i;
@@ -99,9 +101,6 @@ struct binomial_coefficient_prime_power_mod {
     {
         constexpr std::pair<u32,u32> pq = self::_factorize(mod_type::mod());
         std::tie(this->_p, this->_q) = pq;
-
-        this->_barrett_m = barrett_32bit(this->_m);
-        this->_barrett_p = barrett_32bit(this->_p);
 
         this->_init();
     }
@@ -126,7 +125,7 @@ struct binomial_coefficient_prime_power_mod {
     inline mod_type mod() const noexcept(NO_EXCEPT) { return this->_m; }
 
     mod_type lucus(value_type n, value_type k) const noexcept(NO_EXCEPT) {
-        u32 res = 1;
+        mint res = mint::one;
 
         while(n > 0) {
             u32 n0, k0;
@@ -134,13 +133,12 @@ struct binomial_coefficient_prime_power_mod {
             std::tie(k, k0) = this->_barrett_p.divide(static_cast<u64>(k));
             if(n0 < k0) return 0;
 
-            res = this->_barrett_m.remainder(static_cast<u64>(res) * this->_fact[n0]);
-
-            const u32 tmp = this->_barrett_m.remainder(static_cast<u64>(this->_inv_fact[n0 - k0]) * this->_inv_fact[k0]);
-            res = this->_barrett_m.remainder(static_cast<u64>(res) * tmp);
+            res *= this->_fact[n0];
+            res *= this->_inv_fact[n0 - k0];
+            res *= this->_inv_fact[k0];
         }
 
-        return static_cast<mod_type>(res);
+        return static_cast<mod_type>(res.val());
     }
 
     mod_type comb(value_type n, value_type k) const noexcept(NO_EXCEPT) {
@@ -149,12 +147,12 @@ struct binomial_coefficient_prime_power_mod {
 
         value_type r = n - k;
         u32 e0 = 0, eq = 0, i = 0;
-        u32 res = 1;
+        mint res = mint::one;
 
         while(n > 0) {
-            res = this->_barrett_m.remainder(static_cast<u64>(res) * this->_fact[this->_barrett_m.remainder(static_cast<u64>(n))]);
-            res = this->_barrett_m.remainder(static_cast<u64>(res) * this->_inv_fact[this->_barrett_m.remainder(static_cast<u64>(k))]);
-            res = this->_barrett_m.remainder(static_cast<u64>(res) * this->_inv_fact[this->_barrett_m.remainder(static_cast<u64>(r))]);
+            res * this->_fact[this->_barrett_m.remainder(static_cast<u64>(n))];
+            res *= this->_inv_fact[this->_barrett_m.remainder(static_cast<u64>(k))];
+            res *= this->_inv_fact[this->_barrett_m.remainder(static_cast<u64>(r))];
 
             n = this->_barrett_p.quotient(static_cast<u64>(n));
             k = this->_barrett_p.quotient(static_cast<u64>(k));
@@ -166,10 +164,10 @@ struct binomial_coefficient_prime_power_mod {
             if(++i >= this->_q) eq += eps;
         }
 
-        if(eq & 1) res = this->_barrett_m.remainder(static_cast<u64>(res) * this->_delta);
-        res = this->_barrett_m.remainder(static_cast<u64>(res) * this->_barrett_m.pow(static_cast<u64>(this->_p), static_cast<u64>(e0)));
+        if(eq & 1) res *= this->_delta;
+        res *= mint::raw(this->_q == 1 : 0 ? this->p).pow(e0);
 
-        return static_cast<mod_type>(res);
+        return static_cast<mod_type>(res.val());
     }
 };
 
