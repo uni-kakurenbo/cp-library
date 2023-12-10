a6a946c682639ca45039e0522934ed961a304764
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-10 20:51:53 +0900

    add-verify: matrix determinant

---
+++ verify/matrix-determinant/00-01.test.cpp
@@ -0,0 +1,26 @@
+/*
+ * @uni_kakurenbo
+ * https://github.com/uni-kakurenbo/competitive-programming-workspace
+ *
+ * CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+/* #language C++ 20 GCC */
+
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_det_arbitrary_mod"
+
+#include "sneaky/enforce_int128_enable.hpp"
+
+#include <iostream>
+#include "snippet/fast_io.hpp"
+#include "adapter/io.hpp"
+#include "numeric/modint.hpp"
+#include "numeric/matrix.hpp"
+
+using mint = lib::arbitrary_montgomery_modint_32bit<-1>;
+
+signed main() {
+    int n, m; input >> n >> m;
+    mint::set_mod(m);
+    lib::matrix<mint> a(n, n); input >> a;
+    print(a.determinant());
+}
