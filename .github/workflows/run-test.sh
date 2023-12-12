2afc96e44d07f308ef3dcb3b502fb813185529ff
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-12 21:12:54 +0900

    modify: workflows

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -6,12 +6,13 @@ TARGET="$1"
 DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET")
 
 #shellcheck disable=SC2086
-LAST_COMMIT_DATE="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
+LAST_MODIFY_DATE="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
 LAST_VERIFY_DATE="$(jq -r --arg target "$TARGET" --arg default @0 '.[$target] // $default' ./.verify-helper/timestamps.remote.json)"
-LAST_COMMITTED_AT=$(date --date "$LAST_COMMIT_DATE" "+%s")
+LAST_MODIFIED_AT=$(date --date "$LAST_MODIFY_DATE" "+%s")
 LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" "+%s")
 
-if [ "$LAST_COMMITTED_AT" -le "$LAST_VERIFIED_AT" ]; then
+echo "$LAST_MODIFIED_AT $LAST_VERIFIED_AT"
+if [ "$LAST_MODIFIED_AT" -le "$LAST_VERIFIED_AT" ]; then
   echo "already verified."
 else
     oj-verify run "$TARGET" --tle 5 || exit 1
@@ -19,6 +20,6 @@ fi
 
 cd .verify-helper || exit 1
 
-# jq -n --arg target "$TARGET" --arg date "$LAST_COMMIT_DATE" '.[$target] = $date' >> ./timestamps-"$NODE_ID".json
-echo "{}" > ./timestamps-"$NODE_ID".json
+jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" '.[$target] = $date' >> ./timestamps-"$NODE_ID".json
+# echo "{}" > ./timestamps-"$NODE_ID".json
 cat ./timestamps-"$NODE_ID".json
