0b49dd1683d2a435e864c8d1f8c8b457dda59c80
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-13 08:11:13 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 
 WORKING_DIRECTORY="$PWD"
 TARGET="$1"
@@ -13,22 +14,22 @@ LAST_VERIFY_DATE="$(
     '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json
 )"
 
-LAST_MODIFIED_AT=$(date --date "$LAST_MODIFY_DATE" "+%s")
-LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" "+%s")
+LAST_MODIFIED_AT=$(date --date "$LAST_MODIFY_DATE" '+%s')
+LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" '+%s')
 
 {
     echo "::group::$TARGET [PID: $PID]"
-    echo "Last modified: $LAST_MODIFY_DATE ($LAST_MODIFIED_AT)"
-    echo "Last verified: $LAST_VERIFY_DATE ($LAST_VERIFIED_AT)"
+    echo "Last modify: $LAST_MODIFY_DATE ($LAST_MODIFIED_AT)"
+    echo "Last verify: $LAST_VERIFY_DATE ($LAST_VERIFIED_AT)"
 
     if [ "$LAST_MODIFIED_AT" -le "$LAST_VERIFIED_AT" ]; then
         echo "Already verified."
     else
-        oj-verify run "$TARGET" --tle 5 || exit 1
+        oj-verify run "$TARGET" --tle 5
     fi
 
     echo "::endgroup::"
-} &>> ".log-$PID.txt" || exit 1
+} &>> ".log-$PID.txt"
 
 jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" \
 '.[$target] = $date' >> "./.verify-helper/timestamps-$NODE_ID.json"
