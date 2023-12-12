5f7a7d3f3fea4f4524eef8b7b71bbfd2d4fe00ab
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-12 15:31:49 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -4,7 +4,8 @@ TARGET="$1"
 echo "$BASE_DIR"
 DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET")
 LAST_COMMIT="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
-date --date "LAST_COMMIT" "+%s"
+echo "$LAST_COMMIT"
+date --date "$LAST_COMMIT" "+%s"
 
 oj-verify run "$TARGET" --tle 5 || exit 1
 cd .verify-helper
