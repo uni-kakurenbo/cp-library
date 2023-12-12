169ef8b688e72c9fe792dbc1564b8f2b662cc90c
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-12 15:20:15 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -1,9 +1,9 @@
 BASE_DIR="$PWD"
 TARGET="$1"
 
-
 echo "$BASE_DIR"
-g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET"
+DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET")
+git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES
 
 oj-verify run "$TARGET" --tle 5 || exit 1
 cd .verify-helper
