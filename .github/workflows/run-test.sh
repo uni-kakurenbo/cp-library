a04d59aa5e8ef65ce7190adcba9788652e79fcd1
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-12 22:43:55 +0900

    modify: workflows

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -1,29 +1,28 @@
 #! /bin/bash
 
-BASE_DIR="$PWD"
 TARGET="$1"
-
-
-cd "$BASE_DIR" || exit 1
-
-
-DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET")
+DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$GITHUB_WORKSPACE" "$TARGET")
 
 #shellcheck disable=SC2086
 LAST_MODIFY_DATE="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
-LAST_VERIFY_DATE="$(jq -r --arg target "$TARGET" '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json)"
+LAST_VERIFY_DATE="$(
+    jq -r --arg target "$TARGET" \
+    '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json
+)"
+
 LAST_MODIFIED_AT=$(date --date "$LAST_MODIFY_DATE" "+%s")
 LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" "+%s")
 
-echo "$LAST_MODIFIED_AT $LAST_VERIFIED_AT"
+echo "$TARGET"
+echo "Last modified: $LAST_MODIFY_DATE ($LAST_MODIFIED_AT)"
+echo "Last verified: $LAST_VERIFY_DATE ($LAST_VERIFIED_AT)"
+
 if [ "$LAST_MODIFIED_AT" -le "$LAST_VERIFIED_AT" ]; then
-  echo "already verified."
+  echo "Already verified."
 else
     oj-verify run "$TARGET" --tle 5 || exit 1
 fi
 
 cd .verify-helper || exit 1
-
-# jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" '.[$target] = $date' >> ./timestamps-"$NODE_ID".json
-echo "{}" > ./timestamps-"$NODE_ID".json
-# cat ./timestamps-"$NODE_ID".json
+jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" \
+'.[$target] = $date' >> ./timestamps-"$NODE_ID".json
