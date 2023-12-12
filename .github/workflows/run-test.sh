bb70b1bdb3ef03e2255510028a6d835840bd9f2f
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-12 15:43:48 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -3,9 +3,16 @@ TARGET="$1"
 
 echo "$BASE_DIR"
 DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET")
-LAST_COMMIT="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
-echo "$LAST_COMMIT"
-date --date "$LAST_COMMIT" "+%s"
+LAST_COMMIT_DATE="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
+LAST_VERIFY_DATE="$(cat ./.verify-helper/timestamps.remote.json | jq ".$TARGET")"
+LAST_COMMITTED_AT=$(date --date "$LAST_COMMIT_DATE" "+%s")
+LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" "+%s")
+
+echo "$LAST_COMMITTED_AT $LAST_VERIFIED_AT"
+if [ $LAST_COMMITTED_AT -le $LAST_VERIFIED_AT ]; then
+  echo "already verified."
+  exit 0
+fi
 
 oj-verify run "$TARGET" --tle 5 || exit 1
 cd .verify-helper
