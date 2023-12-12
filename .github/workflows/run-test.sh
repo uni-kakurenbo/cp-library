adcc539a70d3a9168a69976bf34ff8cb7e1603aa
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-12 20:50:53 +0900

    modify: workflows

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -7,7 +7,7 @@ DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET")
 
 #shellcheck disable=SC2086
 LAST_COMMIT_DATE="$(git log -1 --date=iso --pretty=%ad -- $DEPENDENCIES)"
-LAST_VERIFY_DATE="$(jq -r --arg target "$TARGET" '.[$target]' ./.verify-helper/timestamps.remote.json)"
+LAST_VERIFY_DATE="$(jq -r --arg target "$TARGET" --arg default @0 '.[$target] // $default' ./.verify-helper/timestamps.remote.json)"
 LAST_COMMITTED_AT=$(date --date "$LAST_COMMIT_DATE" "+%s")
 LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" "+%s")
 
