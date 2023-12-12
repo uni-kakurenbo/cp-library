b9cf9439a900ef5367fb99a548f95ab168afa288
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-12 15:10:38 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -1,9 +1,11 @@
 BASE_DIR="$PWD"
 TARGET="$1"
 
+
+echo "$BASE_DIR"
+g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "$TARGET"
+
 oj-verify run "$TARGET" --tle 5 || exit 1
 cd .verify-helper
-echo "$BASE_DIR"
-g++-12 -std=gnu++20 -MM -I"$BASE_DIR" "./$TARGET" 1 &>
 echo >> ./timestamps.remote.json
 cat ./timestamps.remote.json >> ./timestamps-"$NODE_ID".json
