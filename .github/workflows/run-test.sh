7e177b71f7f623ad2aaf10986e6965ac6e42ef61
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-12 22:14:00 +0900

    modify: workflows

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -4,8 +4,6 @@ BASE_DIR="$PWD"
 TARGET="$1"
 
 
-cd "/home/runner/.cache/online-judge-tools/library-checker-problems/sample/many_aplusb_128bit" && ls
-
 cd "$BASE_DIR" || exit 1
 
 
@@ -26,6 +24,6 @@ fi
 
 cd .verify-helper || exit 1
 
-jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" '.[$target] = $date' >> ./timestamps-"$NODE_ID".json
-# echo "{}" > ./timestamps-"$NODE_ID".json
-cat ./timestamps-"$NODE_ID".json
+# jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" '.[$target] = $date' >> ./timestamps-"$NODE_ID".json
+echo "{}" > ./timestamps-"$NODE_ID".json
+# cat ./timestamps-"$NODE_ID".json
