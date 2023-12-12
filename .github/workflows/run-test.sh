999188f33cb2d49f067c23a3087d868f1ac941cc
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-12 22:55:51 +0900

    modify: workflows

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -14,17 +14,22 @@ LAST_VERIFY_DATE="$(
 LAST_MODIFIED_AT=$(date --date "$LAST_MODIFY_DATE" "+%s")
 LAST_VERIFIED_AT=$(date --date "$LAST_VERIFY_DATE" "+%s")
 
-echo "::group::$TARGET"
-echo "Last modified: $LAST_MODIFY_DATE ($LAST_MODIFIED_AT)"
-echo "Last verified: $LAST_VERIFY_DATE ($LAST_VERIFIED_AT)"
+{
+    echo "::group::$TARGET"
+    echo "Last modified: $LAST_MODIFY_DATE ($LAST_MODIFIED_AT)"
+    echo "Last verified: $LAST_VERIFY_DATE ($LAST_VERIFIED_AT)"
+    echo .
 
-if [ "$LAST_MODIFIED_AT" -le "$LAST_VERIFIED_AT" ]; then
-  echo "Already verified."
-else
-    oj-verify run "$TARGET" --tle 5 || exit 1
-fi
-echo "::endgroup::"
+    if [ "$LAST_MODIFIED_AT" -le "$LAST_VERIFIED_AT" ]; then
+        echo "Already verified."
+    else
+        oj-verify run "$TARGET" --tle 5 || exit 1
+    fi
 
-cd .verify-helper || exit 1
+    echo "::endgroup::"
+} >> .log.txt
+
 jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" \
-'.[$target] = $date' >> ./timestamps-"$NODE_ID".json
+'.[$target] = $date' >> ./.verify-helper/timestamps-"$NODE_ID".json
+
+cat .log
