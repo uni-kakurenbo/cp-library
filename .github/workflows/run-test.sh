05350a086c3d66f65c986c4a5a5194c6eb93cda2
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-13 14:23:43 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -48,7 +48,7 @@ jq -n --arg target "$TARGET" --arg date "$LAST_MODIFY_DATE" \
 cat ".log-$PID.txt"
 
 if [ $EXIT_STATUS -eq 0 ]; then
-    echo "::notice file=$TARGET::Skipped or passed tests successfully."
+    echo "::notice file=$TARGET::Skipped or passed all tests successfully."
 fi
 
 exit $EXIT_STATUS
