5cf360150c065c9f58c3056381487fc8b16f0788
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-18 10:15:17 +0900

    Update concepts.hpp

--- internal/concepts.hpp
+++ internal/concepts.hpp
@@ -51,58 +51,62 @@ concept weakly_multipliation_assignalbe = requires (L lhs, R rhs) { lhs *= rhs; 
 template<class L, class R = L>
 concept weakly_division_assignable = requires (L lhs, R rhs) { lhs /= rhs; };
 
-
-template<class T0, class T1>
-concept reducible =
-    std::common_with<T0, T1> &&
-    std::common_with<T0, std::common_type_t<T0, T1>> &&
-    std::common_with<std::common_type_t<T0, T1>, T1>;
-
-
 template<class L, class R = L>
 concept addable =
-    reducible<L, R> &&
     weakly_addable<L, R> &&
-    requires (L lhs, R rhs) { { lhs + rhs } -> std::same_as<std::common_type_t<L, R>>; };
+    weakly_addable<std::invoke_result_t<std::plus<L, R>&, L, R>, R> &&
+    weakly_addable<L, std::invoke_result_t<std::plus<L, R>&, L, R>> &&
+    weakly_addable<std::invoke_result_t<std::plus<L, R>&, L, R>, std::invoke_result_t<std::plus<L, R>&, L, R>>;
 
 template<class L, class R = L>
 concept subtractable =
-    reducible<L, R> &&
     weakly_subtractable<L, R> &&
-    requires (L lhs, R rhs) { { lhs - rhs } -> std::same_as<std::common_type_t<L, R>>; };
+    weakly_subtractable<std::invoke_result_t<std::minus<L, R>&, L, R>, R> &&
+    weakly_subtractable<L, std::invoke_result_t<std::minus<L, R>&, L, R>> &&
+    weakly_subtractable<std::invoke_result_t<std::minus<L, R>&, L, R>, std::invoke_result_t<std::minus<L, R>&, L, R>>;
 
 template<class L, class R = L>
 concept multipliable =
-    reducible<L, R> &&
     weakly_multipliable<L, R> &&
-    requires (L lhs, R rhs) { { lhs * rhs } -> std::same_as<std::common_type_t<L, R>>; };
+    weakly_multipliable<std::invoke_result_t<std::multiplies<L, R>&, L, R>, R> &&
+    weakly_multipliable<L, std::invoke_result_t<std::multiplies<L, R>&, L, R>> &&
+    weakly_multipliable<std::invoke_result_t<std::multiplies<L, R>&, L, R>, std::invoke_result_t<std::multiplies<L, R>&, L, R>>;
 
 template<class L, class R = L>
 concept divisable =
-    reducible<L, R> &&
     weakly_divisable<L, R> &&
-    requires (L lhs, R rhs) { { lhs / rhs } -> std::same_as<std::common_type_t<L, R>>; };
+    weakly_divisable<std::invoke_result_t<std::divides<L, R>&, L, R>, R> &&
+    weakly_divisable<L, std::invoke_result_t<std::divides<L, R>&, L, R>> &&
+    weakly_divisable<std::invoke_result_t<std::divides<L, R>&, L, R>, std::invoke_result_t<std::divides<L, R>&, L, R>>;
 
 
 template<class L, class R = L>
 concept addition_assignable =
     weakly_addition_assignable<L, R> &&
-    requires (L lhs, R rhs) { { lhs += rhs } -> std::same_as<L&>; };
+    weakly_addition_assignable<std::decay_t<std::invoke_result_t<std::plus<L, R>&, L, R>>, R> &&
+    weakly_addition_assignable<L, std::invoke_result_t<std::plus<L, R>&, L, R>> &&
+    weakly_addition_assignable<std::decay_t<std::invoke_result_t<std::plus<L, R>&, L, R>>, std::invoke_result_t<std::plus<L, R>&, L, R>>;
 
 template<class L, class R = L>
 concept subtraction_assignable =
     weakly_subtraction_assignable<L, R> &&
-    requires (L lhs, R rhs) { { lhs -= rhs } -> std::same_as<L&>; };
+    weakly_subtraction_assignable<std::decay_t<std::invoke_result_t<std::minus<L, R>&, L, R>>, R> &&
+    weakly_subtraction_assignable<L, std::invoke_result_t<std::minus<L, R>&, L, R>> &&
+    weakly_subtraction_assignable<std::decay_t<std::invoke_result_t<std::minus<L, R>&, L, R>>, std::invoke_result_t<std::minus<L, R>&, L, R>>;
 
 template<class L, class R = L>
 concept multipliation_assignalbe =
     weakly_multipliation_assignalbe<L, R> &&
-    requires (L lhs, R rhs) { { lhs *= rhs } -> std::same_as<L&>; };
+    weakly_multipliation_assignalbe<std::decay_t<std::invoke_result_t<std::multiplies<L, R>&, L, R>>, R> &&
+    weakly_multipliation_assignalbe<L, std::invoke_result_t<std::multiplies<L, R>&, L, R>> &&
+    weakly_multipliation_assignalbe<std::decay_t<std::invoke_result_t<std::multiplies<L, R>&, L, R>>, std::invoke_result_t<std::multiplies<L, R>&, L, R>>;
 
 template<class L, class R = L>
 concept division_assignable =
     weakly_division_assignable<L, R> &&
-    requires (L lhs, R rhs) { { lhs /= rhs } -> std::same_as<L&>; };
+    weakly_division_assignable<std::decay_t<std::invoke_result_t<std::divides<L, R>&, L, R>>, R> &&
+    weakly_division_assignable<L, std::invoke_result_t<std::divides<L, R>&, L, R>> &&
+    weakly_division_assignable<std::decay_t<std::invoke_result_t<std::divides<L, R>&, L, R>>, std::invoke_result_t<std::divides<L, R>&, L, R>>;
 
 
 template<class T>
@@ -140,20 +144,34 @@ concept decrementable =
 
 
 template<class L, class R = L>
-concept weakly_arithmetic_operable = weakly_addable<L, R> && weakly_subtractable<L, R> && weakly_multipliable<L, R> && weakly_divisable<L, R>;
+concept weakly_arithmetic_operable =
+    weakly_addable<L, R> &&
+    weakly_subtractable<L, R> &&
+    weakly_multipliable<L, R> &&
+    weakly_divisable<L, R>;
 
 template<class L, class R = L>
 concept weakly_arithmetic_operation_assignable =
-    weakly_addition_assignable<L, R> && weakly_subtraction_assignable<L, R> &&
-    weakly_multipliation_assignalbe<L, R> && weakly_division_assignable<L, R>;
+    weakly_addition_assignable<L, R> &&
+    weakly_subtraction_assignable<L, R> &&
+    weakly_multipliation_assignalbe<L, R> &&
+    weakly_division_assignable<L, R>;
 
 template<class L, class R = L>
-concept arithmetic_operable = addable<L, R> && subtractable<L, R> && multipliable<L, R> && divisable<L, R>;
+concept arithmetic_operable =
+    weakly_arithmetic_operable<L, R> &&
+    addable<L, R> &&
+    subtractable<L, R> &&
+    multipliable<L, R> &&
+    divisable<L, R>;
 
 template<class L, class R = L>
 concept arithmetic_operation_assignable =
-    addition_assignable<L, R> && subtraction_assignable<L, R> &&
-    multipliation_assignalbe<L, R> && division_assignable<L, R>;
+    weakly_arithmetic_operation_assignable<L, R> &&
+    addition_assignable<L, R> &&
+    subtraction_assignable<L, R> &&
+    multipliation_assignalbe<L, R> &&
+    division_assignable<L, R>;
 
 
 template<class T>
