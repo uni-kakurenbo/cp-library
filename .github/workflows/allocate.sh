1d3dfd2b82f41a5998518252d39b960a4c947c32
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-19 22:15:28 +0900

    modify workflows: allocate.sh

--- .github/workflows/allocate.sh
+++ .github/workflows/allocate.sh
@@ -7,7 +7,7 @@ PID="$$"
 {
   DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"${WORKING_DIRECTORY}" "${TARGET}")
 
-  if [[ $(cat "${TARGET}") =~ ^\#define[^\S]*PROBLEM[^\S]*\"(\S*)\"$ ]]; then
+  if [[ "$(cat "${TARGET}")" =~ \#define[^\S]*PROBLEM[^\S]*\"(\S*)\" ]]; then
     echo "Problem: ${BASH_REMATCH[1]}"
   fi
 
