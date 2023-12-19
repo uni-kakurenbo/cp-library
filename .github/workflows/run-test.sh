1c09bdf0eb2b0966b08853b70a1ab5ba6498c95f
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2023-12-19 12:14:24 +0900

    Update run-test.sh

--- .github/workflows/run-test.sh
+++ .github/workflows/run-test.sh
@@ -9,21 +9,36 @@ DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"${WORKING_DIRECTORY}" "${TARGET}")
 
 #shellcheck disable=SC2086
 LAST_MODIFY_DATE="$(git log -1 --date=iso --pretty=%ad -- ${DEPENDENCIES})"
+LAST_VERIFY_DATE="$(
+    jq -r --arg target "${TARGET}" \
+    '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json
+)"
+
 LAST_MODIFIED_AT=$(date --date "${LAST_MODIFY_DATE}" '+%s')
+LAST_VERIFIED_AT=$(date --date "${LAST_VERIFY_DATE}" '+%s')
 
 EXIT_STATUS=0
 
 set +e
 {
-    echo "::group::oj-verify run"
-
-    oj-verify run "${TARGET}" --tle 30
-    EXIT_STATUS=$?
-
+    echo "::group::${TARGET} [PID: ${PID}]"
+    echo "Last modify: ${LAST_MODIFY_DATE} (${LAST_MODIFIED_AT})"
+    echo "Last verify: ${LAST_VERIFY_DATE} (${LAST_VERIFIED_AT})"
     echo "::endgroup::"
 
-    if [ ${EXIT_STATUS} -eq 0 ]; then
-        echo "::notice file=${TARGET}::All tests passed successfully."
+    if [ "${LAST_MODIFIED_AT}" -le "${LAST_VERIFIED_AT}" ]; then
+        echo "::notice file=${TARGET}::Already verified. (Test was skipped.)"
+    else
+        echo "::group::oj-verify run"
+
+        oj-verify run "${TARGET}" --tle 30
+        EXIT_STATUS=$?
+
+        echo "::endgroup::"
+
+        if [ ${EXIT_STATUS} -eq 0 ]; then
+            echo "::notice file=${TARGET}::All tests passed successfully."
+        fi
     fi
 
     echo
