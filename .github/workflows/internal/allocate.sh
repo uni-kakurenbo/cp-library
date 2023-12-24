6a24071de7b0b7391e09b21a57096d2faf84c1e1
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-24 22:33:23 +0900

    modify workflows: verify

--- .github/workflows/internal/allocate.sh
+++ .github/workflows/internal/allocate.sh
@@ -27,7 +27,7 @@ PID="$$"
   echo '::endgroup::'
 
   if [ "${LAST_MODIFIED_AT}" -le "${LAST_VERIFIED_AT}" ]; then
-    echo "- \`${TARGET}\` (${PROBLEM})" >> "${GITHUB_STEP_SUMMARY}"
+    echo "- [\`${TARGET}\`](https://github.com/${GITHUB_REPOSITORY}/tree/${GITHUB_REF_NAME}/${TARGET}): [Problem](${PROBLEM})" >> "${GITHUB_STEP_SUMMARY}"
   else
     echo "${PROBLEM}" >> ./.verify-helper/problems.txt
     echo "${TARGET} ${PROBLEM} ${LAST_MODIFIED_AT}" >> ./.verify-helper/tests.txt
