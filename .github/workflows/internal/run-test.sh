992991fcf7aed4fbd1a3c7969bbe681efa977098
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-24 11:38:36 +0900

    modify workflows: verify

--- .github/workflows/internal/run-test.sh
+++ .github/workflows/internal/run-test.sh
@@ -30,7 +30,7 @@ set +e
 
     #shellcheck disable=SC2086
     time oj test --command "${TARGET}.exe" --directory "../testcases/${PROBLEM_HASH}/" \
-        --tle 30 ${TESTER_OPTION}
+        --tle 600 ${TESTER_OPTION}
     EXIT_STATUS=$?
 
     echo "::endgroup::"
