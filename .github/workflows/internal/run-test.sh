4f3a7bbbee40787b4583550878a3ca655eb5b9ab
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-24 10:07:43 +0900

    modify workflows: verify

--- .github/workflows/internal/run-test.sh
+++ .github/workflows/internal/run-test.sh
@@ -7,18 +7,6 @@ PROBLEM="$2"
 PROBLEM_HASH="$3"
 PID="$$"
 
-DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"${WORKING_DIRECTORY}" "${TARGET}")
-
-#shellcheck disable=SC2086
-LAST_MODIFY_DATE="$(git log -1 --date=iso --pretty=%ad -- ${DEPENDENCIES})"
-LAST_VERIFY_DATE="$(
-    jq -r --arg target "${TARGET}" \
-    '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json
-)"
-
-LAST_MODIFIED_AT=$(date --date "${LAST_MODIFY_DATE}" '+%s')
-LAST_VERIFIED_AT=$(date --date "${LAST_VERIFY_DATE}" '+%s')
-
 EXIT_STATUS=0
 
 # shellcheck source=/dev/null
@@ -31,30 +19,26 @@ set +e
     echo "Last verify: ${LAST_VERIFY_DATE} (${LAST_VERIFIED_AT})"
     echo "::endgroup::"
 
-    if [ "${LAST_MODIFIED_AT}" -le "${LAST_VERIFIED_AT}" ]; then
-        echo "::notice file=${TARGET}::Already verified. (Test was skipped.)"
-    else
-        echo "::group::build"
-        g++-12 "${OPTIONS[@]}" -I"${WORKING_DIRECTORY}" -o "${TARGET}.exe" "${TARGET}" 1>&2
-        echo "::endgroup::"
+    echo "::group::build"
+    time g++-12 "${OPTIONS[@]}" -I"${WORKING_DIRECTORY}" -o "${TARGET}.exe" "${TARGET}"
+    echo "::endgroup::"
 
-        TESTER_OPTION=''
-        if [ -f "../testcases/${PROBLEM_HASH}/checker" ]; then
-            TESTER_OPTION+="--judge-command ../testcases/${PROBLEM_HASH}/checker"
-        fi
+    TESTER_OPTION=''
+    if [ -f "../testcases/${PROBLEM_HASH}/checker" ]; then
+        TESTER_OPTION+="--judge-command ../testcases/${PROBLEM_HASH}/checker"
+    fi
 
-        echo "::group::run test"
+    echo "::group::run test"
 
-        #shellcheck disable=SC2086
-        oj test --command "${TARGET}.exe" --directory "../testcases/${PROBLEM_HASH}/" \
-            --tle 30 ${TESTER_OPTION}
-        EXIT_STATUS=$?
+    #shellcheck disable=SC2086
+    time oj test --command "${TARGET}.exe" --directory "../testcases/${PROBLEM_HASH}/" \
+        --tle 30 ${TESTER_OPTION}
+    EXIT_STATUS=$?
 
-        echo "::endgroup::"
+    echo "::endgroup::"
 
-        if [ ${EXIT_STATUS} -eq 0 ]; then
-            echo "::notice file=${TARGET}::All tests passed successfully."
-        fi
+    if [ ${EXIT_STATUS} -eq 0 ]; then
+        echo "::notice file=${TARGET}::All tests passed successfully."
     fi
 
     echo
