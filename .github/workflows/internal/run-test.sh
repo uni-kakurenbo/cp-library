3647fb89b3ed19a133624917d926ee64849ad9b5
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-26 22:12:47 +0900

    bug-fix

--- .github/workflows/internal/run-test.sh
+++ .github/workflows/internal/run-test.sh
@@ -38,10 +38,10 @@ set +e
 
     RICH_TARGET="[\`${TARGET}\`](https://github.com/${GITHUB_REPOSITORY}/blob/${GITHUB_REF_NAME}/${TARGET})"
     RICH_PROBLEM="[$(basename "${PROBLEM}")](${PROBLEM})"
-    
+
     echo -ne "- ${RICH_TARGET} (${RICH_PROBLEM})\r" > ../summary.txt
     echo "  - Testcase hash: :\`${PROBLEM_HASH}\`" >> ../summary.txt
-        
+
     if [ ${EXIT_STATUS} -eq 0 ]; then
         cat ../summary.txt >> ../passed-tests.txt
     else
