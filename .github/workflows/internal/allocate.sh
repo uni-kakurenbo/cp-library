ed8b3e360a16e400f70a1ffc2a0ac93dea9c25f1
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-12-29 12:31:55 +0900

    refactor

--- .github/workflows/internal/allocate.sh
+++ .github/workflows/internal/allocate.sh
@@ -1,24 +1,26 @@
 #! /bin/bash
 
 WORKING_DIRECTORY="${PWD}"
-TARGET="$1"
+FILE="$1"
+# HASH="$2"
 PID="$$"
 
 {
-  PROBLEM="$(grep -Po '(?<=\#define\ PROBLEM\ ")[^",]+(?=")' "${TARGET}")"
-  DEPENDENCIES=$(g++-12 -std=gnu++20 -MM -I"${WORKING_DIRECTORY}" "${TARGET}")
+  PROBLEM="$(grep -Po '(?<=\#define\ PROBLEM\ ")[^",]+(?=")' "${FILE}")"
+
+DEPENDENCIES="$(time g++-12 -std=gnu++20 -MM -I"${WORKING_DIRECTORY}" "${FILE}")"
 
   # shellcheck disable=SC2086
   LAST_MODIFY_DATE="$(git log -1 --date=iso --pretty=%ad -- ${DEPENDENCIES})"
   LAST_VERIFY_DATE="$(
-      jq -r --arg target "${TARGET}" \
+      jq -r --arg target "${FILE}" \
       '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json
   )"
 
   LAST_MODIFIED_AT=$(date --date "${LAST_MODIFY_DATE}" '+%s')
   LAST_VERIFIED_AT=$(date --date "${LAST_VERIFY_DATE}" '+%s')
 
-  echo "::group::${TARGET} [PID:${PID}]"
+  echo "::group::${FILE} [PID:${PID}]"
   echo "Last modify: ${LAST_MODIFY_DATE} (${LAST_MODIFIED_AT})"
   echo "Last verify: ${LAST_VERIFY_DATE} (${LAST_VERIFIED_AT})"
   echo '::endgroup::'
@@ -27,12 +29,12 @@ PID="$$"
   echo '::endgroup::'
 
   if [ "${LAST_MODIFIED_AT}" -le "${LAST_VERIFIED_AT}" ]; then
-    RICH_TARGET="[\`${TARGET}\`](https://github.com/${GITHUB_REPOSITORY}/blob/${GITHUB_REF_NAME}/${TARGET})"
+    RICH_TARGET="[\`${FILE}\`](https://github.com/${GITHUB_REPOSITORY}/blob/${GITHUB_REF_NAME}/${FILE})"
     RICH_PROBLEM="[$(basename "${PROBLEM}")](${PROBLEM})"
     echo "- ${RICH_TARGET} (${RICH_PROBLEM})" >> ../skipped-tests.txt
   else
     echo "${PROBLEM}" >> ./.verify-helper/problems.txt
-    echo "${TARGET} ${PROBLEM} ${LAST_MODIFIED_AT}" >> ./.verify-helper/tests.txt
+    echo "${FILE} ${PROBLEM} ${LAST_MODIFIED_AT}" >> ./.verify-helper/tests.txt
   fi
 
   echo
