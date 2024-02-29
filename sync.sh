1aa781dd16bcd1b81463c732a61e001fd1b1267f
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-02-29 22:20:52 +0900

    add: timestamp syncer

---
+++ sync.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cd ./.verify-helper/ || exit 1
+
+jq --slurp --sort-keys \
+    '
+        reduce .[] as $item ([]; . + ($item | to_entries))
+        | group_by(.key) | map(max_by(.value)) |
+        from_entries
+    ' \
+    ./timestamps.remote.json \
+    ./timestamps.local.json \
+    >./timestamps.temp.json
+
+cat ./timestamps.temp.json >./timestamps.remote.json
+cat ./timestamps.temp.json >./timestamps.local.json
+
+rm ./timestamps.temp.json
