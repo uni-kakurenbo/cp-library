a6e6ac7777b71eab7eda6f0953916018b950f179
GitHub <noreply@github.com>
2024-03-20 15:46:06 +0000

    [auto-verifier] organization commit 48cabe66129920cc18a391344560c3dd861c3dc2

--- verify/aizu-online-judge/itp1_1_a/0003.test.cpp
+++ verify/aizu-online-judge/itp1_1_a/0003.test.cpp
@@ -8,8 +8,47 @@
 
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
 
-#include "template/standard.hpp"
+#include "sneaky/enforce_int128_enable.hpp"
+
+#include <iostream>
+#include "snippet/aliases.hpp"
+#include "snippet/fast_io.hpp"
+#include "snippet/iterations.hpp"
+#include "adaptor/io.hpp"
+#include "adaptor/map.hpp"
+#include "data_structure/dynamic_sequence.hpp"
+#include "data_structure/treap.hpp"
+#include "action/null.hpp"
+#include "random/engine.hpp"
+#include "utility/timer.hpp"
 
 signed main() {
     print("Hello World");
+
+    lib::dynamic_sequence<lib::i64> data;
+    lib::map<lib::i64, lib::i64> corr;
+
+    debug(data);
+
+    lib::timer timer(1000);
+
+    while(not timer.expired()) {
+        lib::i64 i = lib::randi64();
+        lib::i64 v = lib::randi64();
+        i /= 2;
+        debug(i, v);
+
+        corr[i] = v;
+        data[i] = v;
+
+        debug(corr, data);
+
+        bool ok = true;
+        ITR(i, v, corr) {
+            ok &= data[i].val() == v;
+        }
+        if(!ok) {
+            assert(false);
+        }
+    }
 }
