470543d932d6d14223202cabc8b79ca2ba851b90
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-03-22 16:58:54 +0900

    add: header generator

---
+++ generate-headers.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+from glob import glob
+from pathlib import Path
+
+OUTPUT_DIRECTORY = Path("./include")
+
+INDIVIDUAL = {
+    "action": "actions",
+    "adaptor": "adaptors",
+    "algebraic": "algebraic",
+    "convolution": "convolutions",
+    "data_structure": "data_structures",
+    "geometry": "geometries",
+    "graph": "graph_theory",
+    "hash": "hashes",
+    "iterable": "iterable",
+    "numeric": "numeric",
+    "snippet": "snippets",
+    "view": "views"
+}
+
+ALL = ["random", "utility", "global", "structure"]
+
+
+def generate(dir):
+    content = defaultdict(list)
+
+    for file in sorted(glob(f"./{ dir }/**/*.hpp", recursive=True)):
+        file = Path(file)
+        category = file.relative_to(dir).parent.name
+        if category == "internal": continue
+        if not category: category = "|"
+        content[category].append(f"#include \"{ file }\"\n")
+
+    return "\n".join(map(lambda key: "".join(content[key]), sorted(content.keys())))
+
+
+for dir, header in INDIVIDUAL.items():
+    dir = Path(dir)
+
+    output = open(OUTPUT_DIRECTORY / f"{ header }.hpp")
+    output_file = open(output.name, "w")
+
+    print(dir)
+    output_file.write(f"#pragma once\n\n{ generate(dir) }")
+
+print("all")
+
+output_all = open(OUTPUT_DIRECTORY / "all.hpp")
+output_file_all = open(output_all.name, "w")
+
+content_all = "#pragma once\n\n"
+for dir in map(Path, INDIVIDUAL.keys()):
+    content_all += f"#include \"{ dir }\"\n"
+
+content_all += "\n"
+
+for dir in map(Path, ALL):
+    content_all += generate(dir) + "\n"
+
+output_file_all.write(content_all)
