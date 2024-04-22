5bff559049c9aa54dde415cbc5a13485d106daf1
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-04-22 23:02:16 +0900

    modify

--- numeric/boundary_seeker.hpp
+++ numeric/boundary_seeker.hpp
@@ -29,48 +29,52 @@ struct integal {
   public:
     integal(std::function<bool(T)> validate) noexcept(NO_EXCEPT) : _validate(validate) {}
 
-    template<const bool REVERSE = false>
+    template<bool INVERT = false>
     T bound(const T _ok, const T _ng) const noexcept(NO_EXCEPT) {
         T ok = _ok, ng = _ng;
-        if constexpr(REVERSE) std::swap(ng, ok);
-        while(std::abs(ok-ng) > 1) {
+        if constexpr(INVERT) std::swap(ng, ok);
+
+        while(std::abs(ok - ng) > 1) {
             T mid = ng + (ok - ng) / 2;
-            (this->_validate(mid) ? ok : ng) = mid;
+            (INVERT ^ this->_validate(mid) ? ok : ng) = mid;
         }
+
         return ok;
     }
 
-    template<const bool INVERT = false>
+    template<bool REVERSE = false, bool INVERT = false>
     T bound(const T ok) const noexcept(NO_EXCEPT) {
-        T ng = INVERT ? -1 : 1;
-        while(this->_validate(ok + ng)) ng += ng;
-        return this->bound(ok, ok + ng);
+        assert(INVERT ^ this->_validate(ok));
+
+        T ng = REVERSE ? -1 : 1;
+        while(INVERT ^ this->_validate(ok + ng)) ng += ng;
+        return this->bound<INVERT>(ok, ok + ng);
     }
 
-    template<const bool REVERSE = false>
+    template<bool REVERSE = false, bool INVERT = false>
     T bound() const noexcept(NO_EXCEPT) {
-        return this->bound<REVERSE>(
+        return this->bound<INVERT>(
             REVERSE ?
-            numeric_limits<T>::arithmetic_infinity():
-            numeric_limits<T>::arithmetic_negative_infinity()
+            numeric_limits<T>::arithmetic_infinity() / 2 :
+            numeric_limits<T>::arithmetic_negative_infinity() / 2
         );
     }
 
-    template<const bool REVERSE = false>
+    template<bool INVERT = false>
     T bound_or(const T ok, const T ng, const T proxy) const noexcept(NO_EXCEPT) {
-        const T res = this->bound<REVERSE>(ok, ng);
+        const T res = this->bound<INVERT>(ok, ng);
         return this->_validate(res) ? res : proxy;
     }
 
-    template<const bool INVERT = false>
+    template<bool REVERSE = false, bool INVERT = false>
     T bound_or(const T ok, const T proxy) const noexcept(NO_EXCEPT) {
-        const T res = this->bound<INVERT>(ok);
+        const T res = this->bound<REVERSE, INVERT>(ok);
         return this->_validate(res) ? res : proxy;
     }
 
-    template<const bool REVERSE = false>
+    template<bool REVERSE = false, bool INVERT = false>
     T bound_or(const T proxy) const noexcept(NO_EXCEPT) {
-        const T res = this->bound<REVERSE>();
+        const T res = this->bound<REVERSE, INVERT>();
         return this->_validate(res) ? res : proxy;
     }
 };
@@ -83,48 +87,51 @@ struct floating_point {
   public:
     floating_point(std::function<bool(T)> validate) noexcept(NO_EXCEPT) : _validate(validate) {}
 
-    template<const bool REVERSE = false, const internal::size_t ITERATIONS = 200>
+    template<bool INVERT = false, internal::size_t ITERATIONS = 200>
     T bound(const T _ok, const T _ng) const noexcept(NO_EXCEPT) {
         T ok = _ok, ng = _ng;
-        if constexpr(REVERSE) std::swap(ng, ok);
+        if constexpr(INVERT) std::swap(ng, ok);
+
         REP(ITERATIONS) {
             T mid = ng + (ok - ng) / 2;
-            (this->_validate(mid) ? ok : ng) = mid;
+            (INVERT ^ this->_validate(mid) ? ok : ng) = mid;
         }
         return ok;
     }
 
-    template<const bool REVERSE = false, const internal::size_t ITERATIONS = 200>
+    template<bool REVERSE = false, bool INVERT = false, internal::size_t ITERATIONS = 200>
     T bound(const T ok) const noexcept(NO_EXCEPT) {
-        T ng = ok + REVERSE ? -1 : 1;
-        while(this->_validate(ng)) ng += REVERSE ? -ng : ng;
-        return this->bound<false,ITERATIONS>(ok, ng);
+        assert(INVERT ^ this->_validate(ok));
+
+        T ng = REVERSE ? -1 : 1;
+        while(INVERT ^ this->_validate(ok + ng)) ng += ng;
+        return this->bound<INVERT>(ok, ok + ng);
     }
 
-    template<const bool REVERSE = false, const internal::size_t ITERATIONS = 200>
+    template<bool REVERSE = false, bool INVERT = false, internal::size_t ITERATIONS = 200>
     T bound() const noexcept(NO_EXCEPT) {
-        return this->bound<REVERSE,ITERATIONS>(
+        return this->bound<INVERT, ITERATIONS>(
             REVERSE ?
-            numeric_limits<T>::arithmetic_infinity() :
-            numeric_limits<T>::arithmetic_negative_infinity()
+            numeric_limits<T>::arithmetic_infinity() / 2 :
+            numeric_limits<T>::arithmetic_negative_infinity() / 2
         );
     }
 
-    template<const bool REVERSE = false, const internal::size_t ITERATIONS = 200>
+    template<bool INVERT = false, internal::size_t ITERATIONS = 200>
     T bound_or(const T ok, const T ng, const T proxy) const noexcept(NO_EXCEPT) {
-        const T res = this->bound<REVERSE,ITERATIONS>(ok, ng);
+        const T res = this->bound<INVERT, ITERATIONS>(ok, ng);
         return this->_validate(res) ? res : proxy;
     }
 
-    template<const bool REVERSE = false, const internal::size_t ITERATIONS = 200>
+    template<bool REVERSE = false, bool INVERT = false, internal::size_t ITERATIONS = 200>
     T bound_or(const T ok, const T proxy) const noexcept(NO_EXCEPT) {
-        const T res = this->bound<REVERSE,ITERATIONS>(ok);
+        const T res = this->bound<REVERSE, INVERT, ITERATIONS>(ok);
         return this->_validate(res) ? res : proxy;
     }
 
-    template<const bool REVERSE = false, const internal::size_t ITERATIONS = 200>
+    template<bool REVERSE = false, bool INVERT = false, internal::size_t ITERATIONS = 200>
     T bound_or(const T proxy) const noexcept(NO_EXCEPT) {
-        const T res = this->bound<REVERSE,ITERATIONS>();
+        const T res = this->bound<REVERSE, INVERT, ITERATIONS>();
         return this->_validate(res) ? res : proxy;
     }
 };
