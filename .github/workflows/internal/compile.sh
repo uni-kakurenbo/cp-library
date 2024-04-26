cf4a69f823193014daf8aebf40ba5f618c3b3b04
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-04-26 14:19:14 +0900

    Update compile.sh

--- .github/workflows/internal/compile.sh
+++ .github/workflows/internal/compile.sh
@@ -10,28 +10,46 @@ source ./.github/workflows/internal/options.env
 
 set +e
 {
-    echo "::group::compile: ${TARGET}"
+    echo "::group::${TARGET}"
 
-    time g++-12 "${OPTIONS[@]}" -O0 -I"${WORKING_DIRECTORY}" -I"${WORKING_DIRECTORY}/../ac-library" "${TARGET}" 2>".warn-${PID}.txt"
+    DEPENDENCIES="$(time g++-12 -std=gnu++20 -MM -I"${WORKING_DIRECTORY}" "${FILE}" | sed -E s/^.*\.test\.o:\ //)"
 
-    LINES=$(wc -l ".warn-${PID}.txt" | cut -d' ' -f1)
+    # shellcheck disable=SC2086
+    LAST_MODIFY_DATE="$(git log -1 --date=iso --pretty=%ad -- ${DEPENDENCIES})"
+    LAST_VERIFY_DATE="$(
+        jq -r --arg target "${FILE}" \
+            '.[$target] // "@0"' ./.verify-helper/timestamps.remote.json
+    )"
 
-    if [[ ${LINES} -gt 3 ]]; then
-        cat ".warn-${PID}.txt"
+    LAST_MODIFIED_AT=$(date --date "${LAST_MODIFY_DATE}" '+%s')
+    LAST_VERIFIED_AT=$(date --date "${LAST_VERIFY_DATE}" '+%s')
 
-        {
-            RICH_TARGET="<a href=https://github.com/${GITHUB_REPOSITORY}/blob/${BRANCH_NAME}/${TARGET}><code>${TARGET}</code></a>"
+    echo "Last modify: ${LAST_MODIFY_DATE} (${LAST_MODIFIED_AT})"
+    echo "Last verify: ${LAST_VERIFY_DATE} (${LAST_VERIFIED_AT})"
 
-            echo -ne "<details>\r"
-            echo -ne "<summary>${RICH_TARGET}</summary>\r"
-            echo -ne "<pre>\r"
-            head ".warn-${PID}.txt" -n 25 | tail -n 22 | tr '\n' '\r' | sed -e 's/</\&lt;/g'
-            echo -ne "</pre>\r"
-            echo -e "</details>\r"
-        } >>".summary-${PID}.txt"
+    if [ "${LAST_MODIFIED_AT}" -gt "${LAST_VERIFIED_AT}" ]; then
+        time g++-12 "${OPTIONS[@]}" -O0 -I"${WORKING_DIRECTORY}" -I"${WORKING_DIRECTORY}/../ac-library" "${TARGET}" 2>".warn-${PID}.txt"
+    
+        LINES=$(wc -l ".warn-${PID}.txt" | cut -d' ' -f1)
+    
+        if [[ ${LINES} -gt 3 ]]; then
+            cat ".warn-${PID}.txt"
+    
+            {
+                RICH_TARGET="<a href=https://github.com/${GITHUB_REPOSITORY}/blob/${BRANCH_NAME}/${TARGET}><code>${TARGET}</code></a>"
+    
+                echo -ne "<details>\r"
+                echo -ne "<summary>${RICH_TARGET}</summary>\r"
+                echo -ne "<pre>\r"
+                head ".warn-${PID}.txt" -n 25 | tail -n 22 | tr '\n' '\r' | sed -e 's/</\&lt;/g'
+                echo -ne "</pre>\r"
+                echo -e "</details>\r"
+            } >>".summary-${PID}.txt"
+        fi
     fi
 
     echo "::endgroup::"
+
 } &>".log-${PID}.txt"
 set -e
 
