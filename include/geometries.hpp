c139a978cb843d87a67671cedcd2b6b88da37282
GitHub <noreply@github.com>
2024-05-04 10:43:16 +0000

    [auto-verifier] header-generation commit d1e8ee91cd0ef672fbb926c2361833d7b270e605

--- include/geometries.hpp
+++ include/geometries.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "geometry/arrow.hpp"
 #include "geometry/circle.hpp"
 #include "geometry/convex_hull.hpp"
 #include "geometry/line.hpp"
 #include "geometry/point.hpp"
+#include "geometry/polygon.hpp"
 #include "geometry/segment.hpp"
 #include "geometry/triangle.hpp"
