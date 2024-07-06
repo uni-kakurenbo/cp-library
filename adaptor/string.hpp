9e831c3b25b33d18d7ae4560ad2d8b096cd6b629
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-07-07 00:38:12 +0900

    hash for uni::string

--- adaptor/string.hpp
+++ adaptor/string.hpp
@@ -14,3 +14,18 @@ using string = internal::advanced_container<std::string>;
 
 
 } // namespace uni
+
+
+namespace std {
+
+
+template<>
+struct hash<uni::string> {
+    inline auto operator()(const uni::string& key) const noexcept(NO_EXCEPT) {
+        return std::hash<std::string>{}(static_cast<std::string>(key));
+    }
+};
+
+
+
+}
