abe7a63a05d0adb4e66008355e83a826ca2e2298
GitHub <noreply@github.com>
2024-12-14 04:30:09 +0000

    [workflow] generate expanded headeres 0e44ef4d8c45e92fb20120046cb8eef0d4d84b2e

--- expanded/standard.hpp
+++ expanded/standard.hpp
@@ -349,7 +349,6 @@ namespace uni { template<class Destination = std::ostream> struct output_adaptor
 /* [end]: adaptor/internal/output.hpp*/
 namespace uni { uni::input_adaptor _input; uni::output_adaptor _print; }
 #define input uni::_input
-#define print uni::_print
 /* [end]: adaptor/io.hpp*/
 /* [begin]: adaptor/map.hpp */
 /* [begin]: adaptor/set.hpp */
